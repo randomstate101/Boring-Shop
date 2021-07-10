998f0cc40c547c269f6682a0d456c14784611e62
randomstate101 <randomstate101@gmail.com>
2021-07-10 15:19:46 +0530

    Rating

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { Container } from 'react-bootstrap'
+import Homescreen from './screens/Homescreen'
 
-const App =() => {
+const App = () => {
   return (
     <>
-    <center> <h1>Welcome to Boring Shop</h1> </center>
-    
-    
+      <Header />
+      <main>
+        <Container className='py-3'>
+          <Homescreen />
+        </Container>
+      </main>
+      <Footer />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
